5b918ca9ef22f9aff4e925534d03f972c493b173
riverfife <riverfife@gmail.com>
2018-07-31 18:43:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is River."
+  end
+  
+  get '/hometown' do
+    "My hometown is Portland, OR."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Get Along"
+  end
 end
